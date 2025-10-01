5c36e5569f4e6c0e02e0972cfbd56f3ee329fc7b
fethi okyar <fethio@gmail.com>
2025-10-01 08:17:03 +0300

    Initial commit

---
+++ cmpnt-writing/auto/main.el
@@ -0,0 +1,34 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt" "letterpaper" "twocolumn")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("hyperref" "bookmarks=true" "colorlinks=true" "linkcolor=black" "citecolor=black" "menucolor=black" "urlcolor=black")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "amsmath"
+    "amsfonts"
+    "amssymb"
+    "graphicx"
+    "enumitem"
+    "hyperref")
+   (TeX-add-symbols
+    "nl"
+    "ohm")
+   (LaTeX-add-bibliographies
+    "references"))
+ :latex)
+
